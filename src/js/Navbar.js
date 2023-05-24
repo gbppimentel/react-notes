a22890f5f4418f033928ea99a4eaa7ac2ced1366
gbppimentel <111882471+gbppimentel@users.noreply.github.com>
2023-05-24 20:00:20 +0800

    nav color

--- src/js/Navbar.js
+++ src/js/Navbar.js
@@ -13,7 +13,11 @@ const Navbar = () => {
   };
   return (
     <nav className="Navbar">
-      <h1>React Project: Notes</h1>
+      <h1>
+        <span style={{ color: "#582D92" }}>React Project</span>:{" "}
+        <span style={{ color: "#f1356d" }}>Notes</span>
+      </h1>
+
       <div className="links">
         <a href="/">Home</a>
         <a href="/create-entry" onClick={openModal}>
