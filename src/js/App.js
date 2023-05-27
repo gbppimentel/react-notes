77ed42b8300f61521c116f48b1728a1ef2e6d220
gbppimentel <111882471+gbppimentel@users.noreply.github.com>
2023-05-27 17:27:17 +0800

    Revert "delete comment"
    This reverts commit 17dc08624ddb2ab33cfa2cf1508a1efc5749b8dc.

--- src/js/App.js
+++ src/js/App.js
@@ -1,6 +1,5 @@
 import Navbar from "./Navbar";
 import Journal from "./Journal";
-import Home from "./Home";
 
 function App() {
   // const person = { name: "Ruby", age: 20 };
--- src/js/Journal.js
+++ src/js/Journal.js
@@ -1,43 +1,10 @@
 import React from "react";
-import { useState } from "react";
 import "../css/journal.css";
 
 const Journal = () => {
-  const [blogs, setBlogs] = useState([
-    {
-      title: "My New Life as an Idol",
-      body: "lorem ipsum...",
-      author: "Ruby",
-      id: 1,
-    },
-    {
-      title: "Revenge.. For Ai.",
-      body: "lorem ipsum...",
-      author: "Aqua",
-      id: 2,
-    },
-    {
-      title: "Thoughts on Aqua's Acting",
-      body: "lorem ipsum...",
-      author: "Kana",
-      id: 3,
-    },
-  ]);
-
   return (
     <div className="journal-container">
-      <div className="col-1">
-        <div className="home">
-          {blogs.map((blog) => (
-            <div className="blog-preview" key={blog.id}>
-              <h2>{blog.title}</h2>
-              <p>Written by {blog.author}</p>
-            </div>
-          ))}
-          ;
-        </div>
-        );
-      </div>
+      <div className="col-1"></div>
       <div className="col-2">Journal</div>
     </div>
   );
